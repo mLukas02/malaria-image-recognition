9466a4a756296e4fdcc754b0d86e34c12adef557
Mikhail Lukashevich <mikhail.n.lukashevich@gmail.com>
2024-08-13 12:39:07 +0200

    Add user app and prediction.

--- src/cnnRecognition/utils/common.py
+++ src/cnnRecognition/utils/common.py
@@ -129,13 +129,13 @@ def save_json(path: Path, data: dict):
 #     return f"~ {size_in_kb} KB"
 
 
-# def decodeImage(imgstring, fileName):
-#     imgdata = base64.b64decode(imgstring)
-#     with open(fileName, 'wb') as f:
-#         f.write(imgdata)
-#         f.close()
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
 
 
-# def encodeImageIntoBase64(croppedImagePath):
-#     with open(croppedImagePath, "rb") as f:
-#         return base64.b64encode(f.read())
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
